53cecb785fb39afbd66306e8c3e09534c6f15347
leemchll <149006213+leemchll@users.noreply.github.com>
2026-08-19 13:25:58 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        // key = alphabet count of an anagram 
+        // values = anagrams of the same alphabet count
+        std::map<vector<int>, vector<std::string>> anagramMap = {};
+        
+        for (std::string word: strs) {
+            // Initialize alphabet count to 0
+            vector<int> anagram(26, 0);
+
+            // For each word, determine unique alphabet count
+            for (char l: word) {
+                anagram[l - 'a']++;
+            }
+            
+            // Inserts key based on existence
+            anagramMap[anagram].push_back(word);
+        }
+
+        // Create sublists of anagrams
+        vector<vector<std::string>> sublists;
+
+        for (const auto& pair : anagramMap) {
+            sublists.push_back(pair.second);
+        }
+
+        return sublists;
+
+    }
+};
